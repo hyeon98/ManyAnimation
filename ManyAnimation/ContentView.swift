34a57cc8ae303d58305b7ffb498ab0b50de840dd
hyeon98 <cjstldks@gmail.com>
2021-07-09 23:05:07 +0900

    Initial Commit

---
+++ ManyAnimation/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ManyAnimation
+//
+//  Created by 정현석 on 2021/07/09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ManyAnimation/ManyAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  ManyAnimationApp.swift
+//  ManyAnimation
+//
+//  Created by 정현석 on 2021/07/09.
+//
+
+import SwiftUI
+
+@main
+struct ManyAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
